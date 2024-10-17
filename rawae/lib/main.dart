726dd751c9a3397d4087935a26b67f47e8b6d9fe
Refalammari <abdullahrefal5@gmail.com>
2024-10-17 12:00:09 +0300

    ff

--- rawae/lib/main.dart
+++ rawae/lib/main.dart
@@ -1,18 +1,52 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      home: WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF1E1E1E),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome To Rawae',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // You can add navigation to another screen (e.g., login) here
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange, // Button color
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+              ),
+              child: Text(
+                'Start',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
